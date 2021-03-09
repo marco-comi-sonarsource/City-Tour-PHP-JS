c31a945cd974377698e14d3536f5003a405a7cc3
marco.comi <marco.comi@sonarsource.com>
2021-03-09 18:10:39 +0100

    test

--- src/main/php/Foo/SelectUser.php
+++ src/main/php/Foo/SelectUser.php
@@ -3,7 +3,14 @@
 $email = Yii::app()->request->getParam('email');
 $emailSafe = mysql_escape_string($email);
 
-$sql1 = "SELECT * FROM USERS WHERE email = '$email'";
+if ($email == "test@test.com")
+{
+    $sql1 = "SELECT * FROM USERS WHERE email = '$email'";
+}
+else 
+{
+    $sql1 = "SELECT * FROM USERS_TEST WHERE email = '$email'";
+}
 $sql2 = "SELECT * FROM USERS WHERE email = '$emailSafe'";
 
 mysql_query($sql1);
